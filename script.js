1a3074ed05683b8220ab8d077ef08a82f12d2461
Christopher Demicoli <christopher.demicoli@outlook.com>
2016-08-07 12:51:51 +0200

    Proof of Concept

---
+++ script.js
@@ -0,0 +1,25 @@
+function levelChanged(newLevel, isCharging) {
+    if (isCharging || newLevel > 0.5) {
+        document.getElementById('batteryNormal').style.display = 'block';
+        document.getElementById('batteryLow').style.display = 'none';
+    }
+    else {
+        document.getElementById('batteryNormal').style.display = 'none';
+        document.getElementById('batteryLow').style.display = 'block';
+    }
+}
+
+navigator.getBattery().then(function (battery) {
+    console.log(battery);
+
+    levelChanged(battery.level, battery.charging);
+    battery.addEventListener('levelchange', function () {
+        console.log(battery);
+        levelChanged(battery.level, battery.charging);        
+    });
+
+    battery.addEventListener('chargingchange', function () {
+        console.log(battery);
+        levelChanged(battery.level, battery.charging);        
+    });
+});
